4f19e0722cf10e67ba80bdaf9aeec1facde24416
vuluu2k <vuluu040320@gmail.com>
2023-11-13 23:43:25 +0700

    config nvim

---
+++ lua/config/init.lua
@@ -0,0 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require('config.globals')
+
+local opts = {
+	defaults = {
+		lazy = true
+	},
+	install = {
+		colorschema = {"gruvbox"}
+	},
+	rtp = {
+		disabled_plugins = {
+	        	-- "gzip",
+        		-- "matchit",
+        		-- "matchparen",
+        		-- "netrwPlugin",
+        		-- "tarPlugin",
+        		-- "tohtml",
+        		-- "tutor",
+        		-- "zipPlugin",
+		}
+	}
+}
+
+require("lazy").setup('plugins')
