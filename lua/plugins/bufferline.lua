3ff7717e1b7b295f96bf26358f56c60cbf94b6af
vuluu2k <vuluu040320@gmail.com>
2023-11-20 00:26:48 +0700

    add theme icon

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,6 @@
+vim.opt.termguicolors = true
+return {
+  'akinsho/bufferline.nvim', 
+  version = "*", 
+  dependencies = 'nvim-tree/nvim-web-devicons'
+}
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,9 +1,19 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local config = {
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+
+}
+
 return {
-  "nvim-tree/nvim-tree.lua",
+  "DaikyXendo/nvim-tree.lua",
   lazy = false,
-  config = {
-    filters = {
-      dot
-    }
-  }
+  config = config,
 }
